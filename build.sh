b56e5f7ea1d68e3f446c943a5b89f08e440b0660
heirro <vavaheirro@live.com>
2022-02-07 16:37:52 +0700

    add js obfuscator

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 echo "Building..."
 rm game.js
-uglifyjs lib/phaserjs/phaser.min.js lib/strings/text.js game/scenes/preload.js game/scenes/play.js game/index.js -c -m -o game.js
+uglifyjs lib/strings/text.js lib/phaserjs/phaser.min.js game/scenes/preload.js game/scenes/play.js game/index.js -c -o game.js
 echo ""
 echo "Done!"
 echo ""
 sleep 1
+echo "Securing the code..."
+javascript-obfuscator 'game.js' -o 'game.js' --config 'lib/compression/obfuscation.json'
+echo "Done!"
+sleep 1
+echo ""
 echo "Packing for github pages..."
 if [ ! -f www.zip ]; 
 		then
